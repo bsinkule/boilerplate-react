6e86e244e5d49219bcdcd2772e9c3872cd2018f8
Brice Sinkule <bsinkule@gmail.com>
2019-01-26 11:18:09 -0700

    2nd loader check, now with outputpath

--- app/config/webpack.base.config.js
+++ app/config/webpack.base.config.js
@@ -20,7 +20,9 @@ module.exports = env => {
               use: [
                 {
                   loader: 'file-loader',
-                  options: {},
+                  options: {
+                    outputPath: 'images',
+                  },
                 },
               ],
             },
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import goalPNG from '../static/images/001-goal.png';
+import flagPNG from '../static/images/002-flag.png';
 
 const Home = () => {
   return (
     <div>
       Home   
       <img src={goalPNG} /> 
+      <img src={flagPNG} /> 
     </div>
   )
 }
